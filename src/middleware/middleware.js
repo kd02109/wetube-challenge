0be4a6ce433280af3118e8048654b73082864776
kd02109 <kd02109@gmail.com>
2022-08-31 15:16:25 +0900

    make own middleware

---
+++ src/middleware/middleware.js
@@ -0,0 +1,33 @@
+export const urlLogger = (req, res, next) => {
+  console.log("Path: ", req.path);
+  return next();
+};
+
+export const timeLogger = (req, res, next) => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+  console.log(`Date: ${year}.${month + 1}.${day}`);
+  return next();
+};
+
+export const securityLogger = (req, res, next) => {
+  const { protocol } = req;
+  let secureStatus;
+  if (protocol === "http") {
+    secureStatus = "Insecure";
+  } else if (protocol === "https") {
+    secureStatus = "Secure";
+  }
+  console.log(secureStatus);
+  return next();
+};
+
+export const protectorMiddelware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.status(404).redirect("/");
+  }
+  return next();
+};
--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 import express from "express";
 import { home, about, contact, login } from "./controller/mainController.js";
+import {
+  protectorMiddelware,
+  securityLogger,
+  timeLogger,
+  urlLogger,
+} from "./middleware/middleware.js";
 
 const app = express();
 const PORT = 4000;
 
 app.set("view engine", "pug");
 app.set("views", "./src/views");
+
+app.use(urlLogger, timeLogger, securityLogger, protectorMiddelware);
 
 app.get("/", home);
 app.get("/about", about);
